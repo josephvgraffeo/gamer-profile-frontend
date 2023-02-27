25c4e42287c6b9c7302c8a7109de3ae6b7e3cc70
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-26 20:56:24 -0500

    ADDED navbar/main profile layout
    added the navbar and main profile layout with grids. Chose to use mui for this project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,22 @@
+import { AppBar, Toolbar, Typography, Stack, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
 export default function Navbar() {
     return (
-        <h1>A Navbar goes here, updated</h1>
+        <AppBar className="navbar" position="static">
+            <Toolbar className="navbar-toolbar" sx={{ justifyContent: 'space-between' }}>
+                <Typography className="navbar-header" variant="h5" component="div">Insert Title Here</Typography>
+                <Stack className="navbar-stack" direction="row" spacing={2}>
+                    <Box className="navbar-box">
+                        <Button className="navbar-button" size="large" variant="text" component={Link} to="/gamerprofile">Profile</Button>
+                            <br />
+                        <Button className="navbar-button" size="large" variant="text" component={Link} to="/account">Account</Button>
+                            <br />
+                        <Button className="navbar-button" size="large" variant="text" component={Link} to="/signin">Logout</Button>
+                    </Box>
+                </Stack>
+            </Toolbar>
+        </AppBar>
     )
 }
--- src/scenes/GamerProfile.js
+++ src/scenes/GamerProfile.js
@@ -1,11 +1,23 @@
+import { Grid, Box } from "@mui/material";
 import GamerCard from "../components/GamerCard.js";
 import GamerLibrary from "../components/GamerLibrary.js";
+import "../styles/profile.css";
 
 export default function GamerProfile() {
     return (
-        <>
-            <GamerCard />
-            <GamerLibrary />
-        </>
+        <Box className="prof-main-box">
+            <Grid className="prof-main-grid" container>
+                <Grid className="prof-card-grid" item xs={12} md={3}>
+                    <Box className="prof-card-box">
+                        <GamerCard />
+                    </Box>
+                </Grid>
+                <Grid className="prof-lib-grid" item xs={12} md={9}>
+                    <Box className="prof-lib-box">
+                        <GamerLibrary />
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
