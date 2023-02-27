021550a87c84b9cca1fb2a852b572ce9a011dd20
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-27 13:06:16 -0500

    UPDATED navbar header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <AppBar className="navbar" position="static">
             <Toolbar className="navbar-toolbar" sx={{ justifyContent: 'space-between' }}>
-                <Typography className="navbar-header" variant="h5" component="div">Insert Title Here</Typography>
+                <Typography className="navbar-header" variant="h5" component="div">My Gameport</Typography>
                 <Stack className="navbar-stack" direction="row" spacing={2}>
                     <Box className="navbar-box">
                         <Button className="navbar-button" size="large" variant="text" component={Link} to="/gamerprofile">Profile</Button>
