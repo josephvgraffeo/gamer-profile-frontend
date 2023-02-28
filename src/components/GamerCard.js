5a0e691bf8d18aace0e8d3074da497b8a222fd4d
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-27 21:18:15 -0500

    ADDED loading bar
    added loading bar while waiting for api to fetch data

--- src/components/GamerCard.js
+++ src/components/GamerCard.js
@@ -1,33 +1,47 @@
 import { useState, useEffect } from "react";
+import { CircularProgress } from "@mui/material";
 import "../styles/gamercard.css";
 
 export default function GamerCard() {
     const [gamerCard, setGamerCard] = useState([]);
-    useEffect(() => {getGamerCard()}, [])
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => { getGamerCard() }, [])
     const getGamerCard = () => {
         fetch('https://gamer-profile-project.web.app/gamerCard')
             .then(res => res.json())
-            .then(data => setGamerCard(data))
+            .then(data => {
+                setGamerCard(data);
+                setIsLoading(false);
+            })
             .catch(error => console.error(error))
     }
 
     return (
-        <div>
-            {gamerCard.map(gamerCard => (
-                <div key={gamerCard.id}>
-                    <img className="card-image" src={gamerCard.profile_pic} alt="profile pic" />
-                    <div className="card-section-div">
-                        <h2>{gamerCard.name}</h2>
-                    </div>
-                    <div className="card-section-div">
-                        <h4>{gamerCard.about}</h4>
-                    </div>
-                    <div className="card-section-div">
-                        <h4>Battle.net - {gamerCard.gamertags.battlenet}</h4>
-                        <h4>Steam ID - {gamerCard.gamertags.steam}</h4>
-                    </div>
+        <>
+            {isLoading
+                ? <CircularProgress
+                    color="secondary"
+                    size={100}
+                    style={{ position: 'absolute', top: '35%', left: '35%', transform: 'translate(-50%, -50%)' }}
+                    />
+                : <div>
+                    {gamerCard.map(gamerCard => (
+                        <div key={gamerCard.id}>
+                            <img className="card-image" src={gamerCard.profile_pic} alt="profile pic" />
+                            <div className="card-section-div">
+                                <h2>{gamerCard.name}</h2>
+                            </div>
+                            <div className="card-section-div">
+                                <h4>{gamerCard.about}</h4>
+                            </div>
+                            <div className="card-section-div">
+                                <h4>Battle.net - {gamerCard.gamertags.battlenet}</h4>
+                                <h4>Steam ID - {gamerCard.gamertags.steam}</h4>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
+            }
+        </>
     )
 }
--- src/components/GamerLibrary.js
+++ src/components/GamerLibrary.js
@@ -1,25 +1,38 @@
 import { useState, useEffect } from "react";
-import { Grid } from "@mui/material";
+import { Grid, CircularProgress } from "@mui/material";
 import "../styles/gamelibrary.css"
 
 export default function GamerLibrary() {
     const [games, setGames] = useState([]);
-    useEffect(() => {getGames();}, [])
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => { getGames(); }, [])
     const getGames = () => {
         fetch('https://gamer-profile-project.web.app/games')
             .then(res => res.json())
-            .then(data => setGames(data))
+            .then(data => {
+                setGames(data);
+                setIsLoading(false);
+            })
             .catch(error => console.error(error))
     }
 
     return (
-        <Grid className="library-grid-container" container spacing={2} direction="row">
-            {games.map(games => (
-                <Grid key={games.id} item xs={12} sm={6} md={4} lg={3}>
-                    <img className="game-cover" src={games.cover_image} alt="cover pic" />
-                    <h3>{games.title}</h3>
+        <>
+            {isLoading
+                ? <CircularProgress
+                    color="secondary"
+                    size={100}
+                    style={{ position: 'absolute', top: '40%', left: '58%', transform: 'translate(-50%, -50%)' }}
+                />
+                : <Grid className="library-grid-container" container spacing={2} direction="row">
+                    {games.map(games => (
+                        <Grid key={games.id} item xs={12} sm={6} md={4} lg={3}>
+                            <img className="game-cover" src={games.cover_image} alt="cover pic" />
+                            <h3>{games.title}</h3>
+                        </Grid>
+                    ))}
                 </Grid>
-            ))}
-        </Grid>
+            }
+        </>
     )
 }
