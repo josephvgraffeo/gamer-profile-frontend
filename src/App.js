2fe800a842b46b8da0738d50b55d454db57a589f
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-03-01 15:53:35 -0500

    framed out login/signup pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <>
       <Navbar />
         <Routes>
-          <Route path='/' element={<Signin />}/>
+          <Route path='/' element={<Signup />}/>
           <Route path='/signup' element={<Signup />}/>
           <Route path='/login' element={<Login />}/>
           <Route path='/gamerprofile' element={<GamerProfile />}/>
