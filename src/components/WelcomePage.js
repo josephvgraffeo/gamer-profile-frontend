2e53c758a39dab82e3e7a258694e0555efcfb14b
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-03-08 11:10:54 -0500

    UPDATED styling for account/contact pages

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material"
+import { Button } from "@mui/material"
 import { Link } from "react-router-dom"
 import "../styles/welcome.css";
 
@@ -11,11 +11,15 @@ export default function WelcomePage() {
                 <h3>Track what you're playing</h3>
                 <h3>Connect with your friends</h3>
                 <h3>Level up your gaming experience</h3>
-                <div style={{ display: "inline" }}>
-                    <p>Already have an account? Login here!<Button className="welcome-page-button" component={Link} to="/login">Login</Button></p>
-                </div>
-                <div style={{ display: "inline" }}>
-                    <p>New user? Create an account!<Button className="welcome-page-button" component={Link} to="/signup">Signup</Button></p>
+                <div style={{ display: "flex" }}>
+                    <div style={{ display: "inline", margin: 15 }}>
+                        <p>Already have an account? Login here!</p>
+                        <Button className="welcome-page-button" component={Link} to="/login">Login</Button>
+                    </div>
+                    <div style={{ display: "inline", margin: 15 }}>
+                        <p>New user? Create an account!</p>
+                        <Button className="welcome-page-button" component={Link} to="/signup">Signup</Button>
+                    </div>
                 </div>
             </div>
         </div>
